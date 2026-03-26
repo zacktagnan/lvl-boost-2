8dd82e5c447d6d167184bc506ef693415577914b
zacktagnan <zacktagnan@gmail.com>
2026-03-26 01:37:41 +0100

    chore: initial config on .env and AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,21 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        // Corrección: siempre activos
+        Model::preventSilentlyDiscardingAttributes();
+        Model::preventAccessingMissingAttributes();
+
+        // Rendimiento: solo en desarrollo
+        Model::preventLazyLoading(!$this->app->isProduction());
     }
 }
